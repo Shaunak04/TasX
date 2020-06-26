ea78379d7ba68af528f33267835c5cd4bfd92665
Shaunak Deshpande <shaunak.deshpande4@gmail.com>
2020-06-26 23:59:21 +0530

    required libraries/packages

---
+++ required.bat
@@ -0,0 +1,7 @@
+python -m pip install win10toast
+python -m pip install pandas
+python -m pip install tk
+python -m pip install pillow
+python -m pip install pyttk
+python -m pip install SpeechRecognition
+python -m pip install pywhatkit
